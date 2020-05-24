d6d2bb2c88ca6f5cf566b917ff407c109638102f
Vasu Telaprolu <Vasubabu.Telaprolu@aexp.com>
2020-05-24 16:34:28 -0700

    time/infinite tests

--- src/test/java/com/vt/learnreactor/FluxAndMonoWithTimeTest.java
+++ src/test/java/com/vt/learnreactor/FluxAndMonoWithTimeTest.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 public class FluxAndMonoWithTimeTest {
 
     @Test
-    public void infiniteSequence(){
+    public void infiniteSequence() {
         Flux<Long> longFlux = Flux.interval(Duration.ofMillis(200));
 
         longFlux.subscribe(System.out::println);
@@ -19,5 +19,26 @@ public class FluxAndMonoWithTimeTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void infiniteSequenceTest() {
+        Flux<Long> longFlux = Flux.interval(Duration.ofMillis(200))
+                .take(5);
+        StepVerifier.create(longFlux.log())
+                .expectSubscription()
+                .expectNext(0L, 1L, 2L, 3L, 4L)
+                .verifyComplete();
+    }
+
+    @Test
+    public void infiniteSequenceMap() {
+        Flux<Integer> longFlux = Flux.interval(Duration.ofMillis(200))
+                .map(Long::intValue)
+                .take(3);
+        StepVerifier.create(longFlux.log())
+                .expectSubscription()
+                .expectNext(0, 1, 2)
+                .verifyComplete();
     }
 }
